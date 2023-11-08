8c5e869a60ae3be03190b21857eed372f1406c12
Sthefany Conceição Duquini <88401111+SthefanyConceicaoDuquini@users.noreply.github.com>
2023-11-08 14:19:00 -0400

    Add files via upload

---
+++ supernode.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	libp2p "github.com/libp2p/go-libp2p"
+)
+
+// Implemente os passos do seu sistema P2P aqui
+
+type MasterNode struct {
+	superNodeList []string
+}
+
+func (m *MasterNode) registerSupernode(superNodeID string) {
+	m.superNodeList = append(m.superNodeList, superNodeID)
+}
+
+func configurandoSuperNodo(ctx context.Context, master *MasterNode, superNodeID string) {
+	superNode, err := libp2p.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+	master.registerSupernode(superNodeID)
+
+}
+
+func main() {
+	// Configuração do nó mestre
+	ctx := context.Background()
+	masterNode, err := libp2p.New(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Nó Mestre iniciado:", masterNode.ID())
+
+	// Implemente os passos 1 a 6 conforme mencionado nas especificações do sistema P2P
+
+	// 1) Configuração da Rede Super Nó.
+	// ... implementar o registro dos super nós e o broadcast de finalização ...
+
+	// 2) Configuração da rede do nó servidor.
+	// ... implementar o registro dos nós servidores e o broadcast de roteamento ...
+
+	// 3) Armazenamento dos documentos pdf.
+	// ... implementar a atualização da lista na estrutura de arquivos ...
+
+	// 4) Busca pelos documentos.
+	// ... implementar a lógica de busca e envio de documentos ...
+
+	// 5) Inclusão de novos nós servidores.
+	// ... implementar a lógica para inclusão de novos nós servidores ...
+
+	// 6) Saída de um nó servidor.
+	// ... implementar a lógica para remoção de um nó servidor ...
+
+	// Testes e experimentos
+	// ... implementar testes e geração de relatórios, gráficos e tabelas ...
+
+	// Observação importante 1: Implemente o algoritmo de eleição do super nó e a estrutura condicional para lidar com o caso de um super nó ser eleito mestre.
+	// ... implementar o algoritmo de eleição e a lógica para lidar com a eleição do mestre ...
+
+	// Observação importante 2: Implemente o algoritmo de consistência do sistema, como o 2PC ou outras técnicas avançadas conforme necessário.
+	// ... implementar o algoritmo de 2PC para garantir a consistência do sistema ...
+
+	// Simulação de desligamento do nó mestre para iniciar o processo de eleição
+	time.Sleep(5 * time.Second) // Espera de 5 segundos
+	fmt.Println("Nó Mestre desligado. Iniciando o processo de eleição...")
+
+	// ... implementar o código para o processo de eleição do super nó ...
+
+	// ... implementar o algoritmo de 2PC para garantir a consistência do sistema ...
+
+	// ... continuar com a implementação do sistema P2P ...
+
+}
